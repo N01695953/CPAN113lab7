f2603989c1212f73001c4950c341ca64d1ed44a9
Fariah Yusuf <fariahyusuf@Fariahs-MacBook-Pro.local>
2025-04-04 14:09:12 -0400

    Implemented Form events

--- script.js
+++ script.js
@@ -20,3 +20,25 @@ keyboardInput.addEventListener('keyup', function(event) {
         console.log('Enter key  released.');
     }
 });
+
+//Form events 
+let form = document.getElementById('myForm');
+let username = document.getElementById('username');
+let status = document.getElementById('status');
+
+// Handle form submission
+form.addEventListener('submit', function(event) {
+    console.log('Form submitted');
+    event.preventDefault(); // Prevent the actual form submission
+    alert(`Username: ${username.value}, Status: ${status.value}`);
+});
+
+// Handle changes in the select menu
+status.addEventListener('change', function() {
+    console.log('Selection changed to:', this.value);
+});
+
+// Handle real-time input changes in the username field
+username.addEventListener('input', function() {
+    console.log('Input changed: ', this.value);
+});
